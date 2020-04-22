6d84fe31778a20db56eb3ac89b4c91d91ff2ca1e
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-04-22 15:08:40 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :name, :grade, :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+    
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+
+    end
+
+    def grade(grade)
+        @roster.each do |k,v|
+            if k == grade
+                return v
+            end
+        end
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.each do |k,v|
+            sorted_hash[k] = v.sort
+        end
+        return sorted_hash
+    end
+
+
+
+
+end
